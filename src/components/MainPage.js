afc96671d760e947fe9498e3415ffcd1e1f39c3c
NariP <tto9208@naver.com>
2021-04-04 03:25:00 +0900

    add theme

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -74,16 +74,16 @@ export default  function MainPage({history}) {
             <header>
                 <form style={formStyle}>
                     <label htmlFor='keyword' style={{ marginRight: 30 }}>검색하세요</label>
-                    <input type='text' tabIndex='0' name='keyword' id='keyword' style={inputStyle} onChange={e => onChangeHandler(e)}/>
+                    <input type='text' tabIndex='0' name='keyword' id='keyword' placeholder='1글자 이상 검색해주세요.. 오류나요.. 처리 아직 안했어요..' style={inputStyle} onChange={e => onChangeHandler(e)}/>
                     <button type='search' style={searchBtnStyle} onClick={e => onClickHandler(e)}>검색하기</button>
                 </form>
                 <section style={sectionStyle}>
                     <div style={{marginBottom: 10}}>위치 설정 허용해주셔야 기능이 작동합니다!</div>
                     <button style={curLocationBtnStyle} onClick={getLocation}>현재 위치 조회!</button>
-                    <div>{`위도 : ${curLocation.latitude}, 경도: ${curLocation.longitude}`}</div>
+                    <div style={{marginBottom: 10}}>{`위도 : ${curLocation.latitude}, 경도: ${curLocation.longitude}`}</div>
                 </section>
             </header>
-            <TestD3 curLocation={curLocation}/>
+            <TestD3 curLocation={curLocation} />
         </>
     );
 }
--- src/components/TestD3.js
+++ src/components/TestD3.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {MapContainer, Popup, TileLayer} from 'react-leaflet';
+import {MapContainer, Popup, TileLayer, LayersControl} from 'react-leaflet';
 import {useHistory} from 'react-router-dom';
 
 import myPhoto from '../images/me.png';
@@ -8,6 +8,7 @@ import {MyMarker} from "./MyMarker";
 import {useSelector} from "react-redux";
 
 const cameraBtnStyle = {width: '100%', background:'tomato', border: 'none', color: "white", padding: '0.5rem', cursor: 'pointer'}
+const {BaseLayer} = LayersControl;
 
 function TestD3({curLocation}) {
     const {latitude, longitude} = curLocation;
@@ -22,6 +23,7 @@ function TestD3({curLocation}) {
 
     useEffect(() => {
         setImg(success ? success : img);
+
     }, [img]);
 
 
@@ -35,10 +37,38 @@ function TestD3({curLocation}) {
                 zoomControl={false}
             >
                 <ChangeView center={curPosition} zoom={13} />
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+                <LayersControl>
+                    <BaseLayer checked name='Alidade Smooth Dark'>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png'
+                        />
+                    </BaseLayer>
+                    <BaseLayer name='Alidade Smooth'>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
+                        />
+                    </BaseLayer>
+                    <BaseLayer name='Outdoors'>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png'
+                        />
+                    </BaseLayer>
+                    <BaseLayer name='OSM Bright'>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png'
+                        />
+                    </BaseLayer>
+                    <BaseLayer name='Normal'>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}{r}.png'
+                        />
+                    </BaseLayer>
+                </LayersControl>
                 <MyMarker position={curPosition}>
                     <Popup autoPan={false}>
                         <img src={img} alt='myPhoto' style={{width: '100%', border: 'solid tomato', background: "lightcoral", borderRadius: 10}} />
